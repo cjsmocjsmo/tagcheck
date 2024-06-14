bdf08549f6f8479e47dda136b289d97ffff28fa9
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-06-14 03:09:15 -0700

    Refactor genre tag handling in get_tag_info_mp3 function

---
+++ fix.sh
@@ -0,0 +1,197 @@
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicA/AC_DC/Black_Ice/1_15_-_AC_DC_-_Black_Ice_-_Black_Ice.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicA/AC_DC/Family_Jewels/1_14_-_AC_DC_-_Family_Jewels_-_Whole_Lotta_Rosie.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicA/30_Seconds_To_Mars/30_Seconds_To_Mars/1_04_-_30_Seconds_To_Mars_-_30_Seconds_To_Mars_-_Oblivion.mp3";
+
+
+eyeD3 --to-v2.4 -G "Pop" "../PINAS/foomnt/Music/MusicZip/MusicA/ABBA/The_Definitive_Collection/1_08_-_ABBA_-_The_Definitive_Collection_-_Lay_All_Your_Love_On_Me.mp3";
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicM/Madonna/Best_Of_Madonna/2_09_-_Madonna_-_Best_Of_Madonna_-_Nothing_Really_Matters.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicO/Owl_City/Ocean_Eyes/1_06_-_Owl_City_-_Ocean_Eyes_-_Dental_Care.mp3";
+
+
+eyeD3 --to-v2.4 -a "Ozzy Osbourne" -A "Bark At The Moon" -t "You're No Different" -d 1 -n 02 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicO/Ozzy_Osbourne/Bark_At_The_Moon/1_02_-_Ozzy_Osbourne_-_Bark_At_The_Moon_-_Youre_No_Different.mp3";
+
+eyeD3 --to-v2.4 -G "Alternative" "../PINAS/foomnt/Music/MusicZip/MusicP/Plain_White_Ts/Every_Second_Counts/1_05_-_Plain_White_Ts_-_Every_Second_Counts_-_Friends_Dont_Let_Friends_Dial.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicP/Phil_Collins/The_Platinum_Collection/1_12_-_Phil_Collins_-_The_Platinum_Collection_-_Tomorrow_Never_Knows.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicJ/Johnny_Cash/The_Great_Lost_Performances/1_02_-_Johnny_Cash_-_The_Great_Lost_Performances_-_Lifes_Railway_To_Heaven.mp3";
+
+
+eyeD3 --to-v2.4 -G "Country" "../PINAS/foomnt/Music/MusicZip/MusicJ/Jason_Aldean/Lights_Come_On/1_01_-_Jason_Aldean_-_Lights_Come_On_-_Lights_Come_On.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicJ/John_Denver/Windsong/1_02_-_John_Denver_-_Windsong_-_Cowboys_Delight.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicS/Scorpions/Bad_For_Good_The_Very_Best_Of_The_Scorpions/1_16_-_Scorpions_-_Bad_For_Good_The_Very_Best_Of_The_Scorpions_-_Alien_Nation.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicS/Scorpions/World_Wide_Live/1_07_-_Scorpions_-_World_Wide_Live_-_Big_City_Nights.mp3";
+
+
+eyeD3 --to-v2.4 -G "Country" "../PINAS/foomnt/Music/MusicZip/MusicS/Sugarland/Twice_The_Speed_Of_Life/1_01_-_Sugarland_-_Twice_The_Speed_Of_Life_-_Something_More.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicS/Smile_Empty_Soul/Smile_Empty_Soul/1_03_-_Smile_Empty_Soul_-_Smile_Empty_Soul_-_Nowhere_Kids.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicS/Stevie_Ray_Vaughan/Live_Dallas/1_06_-_Stevie_Ray_Vaughan_-_Live_Dallas_-_How_Do_You_Spell_Love.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicK/Kate_Bush/Never_For_Ever/1_09_-_Kate_Bush_-_Never_For_Ever_-_Night_Scented_Stock.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicK/Kate_Bush/The_Red_Shoes/1_06_-_Kate_Bush_-_The_Red_Shoes_-_Lily.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicC/Cher/The_Very_Best_Of_Cher/1_11_-_Cher_-_The_Very_Best_Of_Cher_-_Bang_Bang.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicC/Carrie_Underwood/Some_Hearts/1_08_-_Carrie_Underwood_-_Some_Hearts_-_Starts_With_Goodbye.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/27. Bittersweet.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/23. My Blue Tears.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/17. Heart Of Glass.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/26. Heartbreaker.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/16. Keep On Loving You.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/08. I Want You Back.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/13. Night Moves.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/02. World On Fire.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/11. Baby, I Love Your Way.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/18. Dont Let The Sun Go Down On Me.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/29. Let It Be.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/01. Rockstar.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/14. Wrecking Ball.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/03. Every Breath You Take.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/30. Free Bird.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/19. Tried To Rock And Roll Me.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/25. You’re No Good.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/28. I Dreamed About Elvis.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/06. Long As I Can See The Light.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/22. Bygones.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/24. Whats Up.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/07. Either Or.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/10. Purple Rain.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/20. Stairway To Heaven.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/21. We Are The Champions We Will Rock You.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/15. I Cant Get No Satisfaction.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/09. What Has Rock And Roll Ever Done For You.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/12. I Hate Myself For Loving You.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/05. Magic Man.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Dolly_Parton/Rockstar/04. Open Arms.mp3";
+
+
+eyeD3 --to-v2.4 -G "Rock" "../PINAS/foomnt/Music/MusicZip/MusicD/Duran_Duran/Duran_Duran/1_01_-_Duran_Duran_-_Duran_Duran_-_A_View_To_A_Kill.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicD/Dire_Straits/Brothers_In_Arms/1_09_-_Dire_Straits_-_Brothers_In_Arms_-_Brothers_In_Arms.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicD/David_Lee_Roth/Eat_Em_And_Smile/1_01_-_David_Lee_Roth_-_Eat_Em_And_Smile_-_Yankee_Rose.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicD/Depeche_Mode/Exciter/1_02_-_Depeche_Mode_-_Exciter_-_Shine.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicG/Guns_N_Roses/Guns_N_Roses/1_04_-_Guns_N_Roses_-_Guns_N_Roses_-_Paradise_City.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicG/Gary_Allan/Used_Heart_For_Sale/1_04_-_Gary_Allan_-_Used_Heart_For_Sale_-_Living_In_A_House_Full_Of_Love.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicG/Garth_Brooks/The_Hits/1_07_-_Garth_Brooks_-_The_Hits_-_American_Honky_Tonk_Bar_Association.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicH/Hank_Williams_Jr/Hank_Williams_Jr/1_02_-_Hank_Williams_Jr_-_Hank_Williams_Jr_-_All_My_Rowdy_Friends_Are_Coming_Over_Tonight.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/Toby_Keith/Boomtown/1_10_-_Toby_Keith_-_Boomtown_-_Boomtown.mp3";
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/The_Offspring/Splinter/1_04_-_The_Offspring_-_Splinter_-_Lightning_Rod.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/The_Offspring/Splinter/1_01_-_The_Offspring_-_Splinter_-_Cant_Get_My_Head_Around_You.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/The_Offspring/Rare_Bonus_Songs/1_04_-_The_Offspring_-_Rare_Bonus_Songs_-_Baghdad.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/The_Offspring/1st/1_08_-_The_Offspring_-_1st_-_Kill_The_President.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/The_Beatles/The_Beatles_Live_At_The_BBC/1_22_-_The_Beatles_-_The_Beatles_Live_At_The_BBC_-_Ticket_To_Ride.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/Ted_Nugent/If_You_Cant_Lick_Em/1_01_-_Ted_Nugent_-_If_You_Cant_Lick_Em_-_Cant_Live_With_Em.mp3";
+
+
+eyeD3 --to-v2.4 "../PINAS/foomnt/Music/MusicZip/MusicT/The_Doobie_Brothers/The_Doobie_Brothers/1_10_-_The_Doobie_Brothers_-_The_Doobie_Brothers_-_Without_Love.mp3";
